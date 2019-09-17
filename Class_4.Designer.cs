e6879e6ffb586ae0c6468d53fcaf847418b61b70
Arafatreza28 <arafatrezabitm3@gmail.com>
2019-09-17 19:15:34 +0600

    list

---
+++ Class_4.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace BITM_Works
+{
+    partial class Class_4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.ShowRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(218, 27);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(12, 29);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(166, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // ShowRichTextBox
+            // 
+            this.ShowRichTextBox.Location = new System.Drawing.Point(406, 12);
+            this.ShowRichTextBox.Name = "ShowRichTextBox";
+            this.ShowRichTextBox.Size = new System.Drawing.Size(311, 452);
+            this.ShowRichTextBox.TabIndex = 2;
+            this.ShowRichTextBox.Text = "";
+            // 
+            // Class_4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 476);
+            this.Controls.Add(this.ShowRichTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.addButton);
+            this.Name = "Class_4";
+            this.Text = "Class_4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.RichTextBox ShowRichTextBox;
+    }
+}
---
+++ Class_4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BITM_Works
+{
+    public partial class Class_4 : Form
+    {
+        List<string> userName = new List<string> { };
+        public Class_4()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+            userName.Add(nameTextBox.Text);
+            ShowData();
+        }
+
+
+        private void ShowData()
+        {
+            ShowRichTextBox.Text = "";
+            foreach (string names in userName)
+            {
+                ShowRichTextBox.Text += names + "\n";
+            }
+        }
+    }
+}
